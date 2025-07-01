c952f39fe566368a0680f336ee45ffd13f0ed0d5
Dawid311 <chojna.dawid1@gmail.com>
2025-07-01 10:04:47 +0000

    Navigationskomponente aktualisiert: Hinzufügen von Zustand für aktiven Tab und Übergabe der erforderlichen Props an die Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,18 @@ import { ConnectButton } from "thirdweb/react";
 import { client } from "./client";
 import Navigation from "./Navigation";
 
+import React, { useState } from "react";
+
 export default function Home() {
+  const [activeTab, setActiveTab] = useState<string>("home");
+
   return (
     <main className="min-h-screen flex flex-col bg-zinc-950">
-      <Navigation />
+      {/* Add state for activeTab and pass required props to Navigation */}
+      <Navigation
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
       <section className="flex-1 flex flex-col items-center justify-center pt-24 pb-8">
         <ConnectButton
           client={client}
