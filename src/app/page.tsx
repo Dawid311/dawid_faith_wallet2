279b4116429c20a3229b546fd81ed9d95e416d4d
Dawid311 <chojna.dawid1@gmail.com>
2025-07-01 07:51:50 +0000

    MyTokenInfo-Komponente in die Hauptseite integriert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Navbar from "../components/Navbar";
+import MyTokenInfo from "../components/MyTokenInfo";
 
 export default function Home() {
   return (
     <main className="p-4 min-h-[100vh] flex flex-col items-center container max-w-screen-lg mx-auto">
       <Navbar />
+      <MyTokenInfo />
     </main>
   );
 }
